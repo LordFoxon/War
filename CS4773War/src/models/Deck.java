a2d77eda89b47d003c6812fbc5f15f1d0df90ed4
cyr471 <cyr471@my.utsa.edu>
2017-09-27 19:03:50 -0500

    Changed deck to be able to rig it

--- CS4773War/src/models/Deck.java
+++ CS4773War/src/models/Deck.java
@@ -4,32 +4,37 @@ import java.util.*;
 public class Deck {
 	
 	
-	ArrayList<Card> cards = new ArrayList<>();
+	ArrayList<Card> cards;
 	
 	public Deck() {
-		createDeck();
-		shuffleDeck();
-		printDeck();
+		cards = new ArrayList<>();
+//		createDeck();
+//		shuffleDeck();
+//		printDeck();
 	}
 	
-	private void createDeck() {
+	public void createDeck() {
 		for(Suits suit: Suits.values()){
 			for(Ranks rank: Ranks.values()){
 				cards.add(new Card(suit, rank));
 			}
 		}
-		
+		shuffleDeck();
 	}
 	
 	private void shuffleDeck() {
-		// TODO Auto-generated method stub
 		Collections.shuffle(cards);
 	}
-
-	private void printDeck(){
-		for(Card card: cards){
-			System.out.print(card.suit+" " +card.rank+"\n");
-		}
+	
+	public void addCard(Card card)
+	{
+		cards.add(card);
 	}
+
+//	private void printDeck(){
+//		for(Card card: cards){
+//			System.out.print(card.suit+" " +card.rank+"\n");
+//		}
+//	}
 	
 }
