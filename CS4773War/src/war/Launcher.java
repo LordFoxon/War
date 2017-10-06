5cbe3bee33f8f0f6aaa31d16d433ab79dc01ad1d
bridg8et <bridg8et@172.24.142.69>
2017-10-05 19:57:26 -0500

    deleted my one line of code

--- CS4773War/src/war/Launcher.java
+++ CS4773War/src/war/Launcher.java
@@ -6,7 +6,6 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		Deck mainDeck = new Deck();	
-		System.out.println("Bridget is Aswsome");
 		mainDeck.createDeck();
 		WarVariation2 war1 = new WarVariation2(mainDeck);
 	}
