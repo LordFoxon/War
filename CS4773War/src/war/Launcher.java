8c56f628715e303e646468fd817e8a028c43fb10
bridg8et <bridg8et@172.24.142.69>
2017-10-05 19:56:05 -0500

    added output

--- CS4773War/src/war/Launcher.java
+++ CS4773War/src/war/Launcher.java
@@ -6,6 +6,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		Deck mainDeck = new Deck();	
+		System.out.println("Bridget is Aswsome");
 		mainDeck.createDeck();
 		WarVariation2 war1 = new WarVariation2(mainDeck);
 	}
